4ea30f3c32323d9d134a5a6f0b199f919b3514f8
arloesol <john@arloesol.com>
2019-04-20 14:23:24 +0200

    move p2p static stuff in higher dir

---
+++ precache-manifest.232aed47a2ad26a87114ca347176b725.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0c903c89055d48372164",
+    "url": "/static/js/main.620b79a3.chunk.js"
+  },
+  {
+    "revision": "0960b32045386d75995c",
+    "url": "/static/js/2.c40cc68f.chunk.js"
+  },
+  {
+    "revision": "52bee17744ee8eb423a6831182f18f63",
+    "url": "/index.html"
+  }
+];
